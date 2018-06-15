4da9bc9e3c5d3a32310255cbc0717b017fd1122b
Dan Jarvis <danwakeem@gmail.com>
2018-06-15 12:11:33 -0500

    Fixed parseArgs Params

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const serverlessTest = require('./serverless.test');
 
 if (process.argv.length > 2) {
-  const params = serverlessTest.parseArgs();
+  const params = serverlessTest.parseArgs(0);
   const action = serverlessTest.loadAction();
   if (action.actions) serverlessTest.runAction(action.actions, params);
   else console.log(action.message);
